3575d8d12f6c1f0e0f77e5462142613df5b91bb6
unknown <bkbagchi.dipto@gmail.com>
2025-08-27 23:37:49 +0600

    Navbar markup and style done

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("heart-count")[0].innerHTML = 0;
+document.getElementsByClassName("coin-count")[0].innerHTML = 100;
+document.getElementsByClassName("copy-count")[0].innerHTML = 0;
