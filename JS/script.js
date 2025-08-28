4d0caa32fe04b0c14edb22214cd40079c863e623
unknown <bkbagchi.dipto@gmail.com>
2025-08-28 21:11:57 +0600

    On click on heart, on navbar heart count inc

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,11 @@
-document.getElementsByClassName("heart-count")[0].innerHTML = 0;
-document.getElementsByClassName("coin-count")[0].innerHTML = 100;
-document.getElementsByClassName("copy-count")[0].innerHTML = 0;
+document.getElementById("heart-count").innerHTML = 0;
+document.getElementById("coin-count").innerHTML = 100;
+document.getElementById("copy-count").innerHTML = 0;
+
+const allHeart = document.getElementsByClassName("heart-icon");
+Array.from(allHeart).forEach((heart) => {
+  heart.addEventListener("click", () => {
+    document.getElementById("heart-count").innerHTML =
+      parseInt(document.getElementById("heart-count").innerHTML) + 1;
+  });
+});
